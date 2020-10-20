a34dfc82633ae8d705431ade02487f485bfa6894
jhasuraj <jhasuraj707@gmail.com>
2020-10-20 14:22:22 +0530

    Add files via upload

---
+++ Ambab/Productarrivals/Setup/Patch/Data/AddProductMaxAddToCartQuantyAttribute.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Ambab\Productarrivals\Setup\Patch\Data;
+
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class AddProductMaxAddToCartQuantyAttribute implements DataPatchInterface
+{
+    /**
+     * ModuleDataSetupInterface
+     *
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * EavSetupFactory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * AddProductMaxAddToCartQuantyAttribute constructor.
+     *
+     * @param ModuleDataSetupInterface  $moduleDataSetup
+     * @param EavSetupFactory           $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute('catalog_product', 'max_add_to_cart_qtys', [
+            'type' => 'int',
+            'label' => 'Max Add To Cart Quantity',
+            'input' => 'text',
+            'source' => '',
+            'default' => 1,
+            'global' => ScopedAttributeInterface::SCOPE_STORE,
+            'visible' => true,
+            'used_in_product_listing' => true,
+            'user_defined' => true,
+            'required' => false,
+            'group' => 'General',
+            'sort_order' => 90,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
